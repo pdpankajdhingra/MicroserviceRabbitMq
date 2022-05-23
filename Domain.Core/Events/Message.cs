bb0f938b057a2e7cdf1b18894f3a698e1259f4d0
neharode <neharodemobimech@gmail.com>
2022-05-24 00:56:47 +0530

    first commit

---
+++ Domain.Core/Events/Message.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Core.Events
+{
+	public abstract class Message : IRequest<bool> //Request is expecting a bool variable back
+	{
+		public string MessageType { get; protected set; }
+		protected Message()
+		{
+			MessageType = GetType().Name;
+		}
+	}
+}
---
+++ MicroserviceWithRabbitMq/Infrastructure.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core.Bus;
+using Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infrastructure.IOC
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			services.AddTransient<IEventBus, RabbitMQBus>();
+		}
+	}
+}
